f636f4e3c8f11a3023ccebd9e6b67431862c2ace
Daniel Marroquin <dev.danielmarroquin25@gmail.com>
2022-07-19 23:18:53 -0500

    Add Services

---
+++ src/app/services/pokemon.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Observable } from "rxjs";
+import { HttpClient } from "@angular/common/http";
+import { environment } from "../../environments/environment";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PokemonService {
+
+  constructor(
+    private http:HttpClient
+  ) { }
+
+  getListPokemon(id: number = 3): Observable<any>{
+    const params = {
+      idAuthor: id
+    }
+    return this.http.get<any>(environment.api, { params } )
+  }
+}
